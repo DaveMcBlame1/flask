7e47b1d9d449b1aa59cf713d6b2a1e696264e4e4
DaveMcBlame1 <165719301+DaveMcBlame1@users.noreply.github.com>
2025-03-01 17:23:57 -0600

    testing to see if changes are possible
    hoping this works

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import logging
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+logging.getLogger().addHandler(console_handler)
 
 @app.route('/')
 def index():
