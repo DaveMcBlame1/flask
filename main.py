4129a3fcd49ef33b4c606e1bf3c5691c8b259eb3
DaveMcBlame1 <165719301+DaveMcBlame1@users.noreply.github.com>
2025-03-02 00:02:50 -0600

    ghgfkkjhgjvg

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ users = {}  # Dictionary to track users by their session IDs
 def get_db():
     if 'db' not in g:
         g.db = connection.MySQLConnection(user='root', password='YUtkvAicAsLgKzfucuXPPEAwxdzbsgUK',
-                                          host='mysql.railway.internal')
+                                          host='mysql.railway.internal',
+                                          database='railway')
         cursor = g.db.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS user (
                             id INT AUTO_INCREMENT PRIMARY KEY,
